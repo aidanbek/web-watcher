7f24b1f84033f5af04e188a829f806ab5aeb092b
Aidanbek Kalymbekov <aidanbek@protonmail.com>
2023-05-28 20:59:37 +0600

    refactor

--- src/PlainTextDiffCalculator.php
+++ src/PlainTextDiffCalculator.php
@@ -6,7 +6,6 @@ use App\Models\DiffType;
 use App\Models\DumpDiff;
 use App\Models\Page;
 use App\Models\PageDump;
-use Illuminate\Hashing\HashManager;
 use Jfcherng\Diff\DiffHelper;
 use Jfcherng\Diff\Renderer\RendererConstant;
 
@@ -25,8 +24,9 @@ class PlainTextDiffCalculator
         ];
 
         $rendererOptions = [
-            'cliColorization' => RendererConstant::CLI_COLOR_AUTO,
-            'detailLevel'     => 'char'
+            'cliColorization'   => RendererConstant::CLI_COLOR_AUTO,
+            'outputTagAsString' => true,
+            'detailLevel'       => 'char'
         ];
 
         return DiffHelper::calculate(
@@ -48,8 +48,9 @@ class PlainTextDiffCalculator
         ];
 
         $rendererOptions = [
-            'cliColorization' => RendererConstant::CLI_COLOR_AUTO,
-            'detailLevel'     => 'char'
+            'cliColorization'   => RendererConstant::CLI_COLOR_AUTO,
+            'outputTagAsString' => true,
+            'detailLevel'       => 'char'
         ];
 
         return DiffHelper::calculate(
@@ -61,43 +62,58 @@ class PlainTextDiffCalculator
         );
     }
 
+    public function calculateDiffForDumps(PageDump $oldDump, PageDump $newDump): array
+    {
+        $result = [];
+
+        if ($oldDump->hash === $newDump->hash) {
+            $result['html'] = PlainTextDiffCalculator::EMPTY_HTML;
+            $result['json'] = PlainTextDiffCalculator::EMPTY_JSON;
+            $result['diff_type_id'] = DiffType::NO_CHANGES;
+        } else {
+            $result['json'] = $this->calculateJson($oldDump->raw_pretty_html, $newDump->raw_pretty_html);
+            $result['html'] = $this->calculateHtml($oldDump->raw_pretty_html, $newDump->raw_pretty_html);
+
+            if ($result['json'] === self::EMPTY_JSON || $result['html'] === self::EMPTY_HTML) {
+                $result['diff_type_id'] = DiffType::NO_CHANGES;
+            } else {
+                $result['diff_type_id'] = DiffType::HAS_CHANGES;
+            }
+        }
+
+        return $result;
+    }
+
+    private function calculateDiffForPage(Page $page)
+    {
+        $page->load('lastTwoDumps');
+
+        if ($page->lastTwoDumps->count() === 2) {
+            $oldDump = $page->lastTwoDumps[1];
+            $newDump = $page->lastTwoDumps[0];
+
+            $result = $this->calculateDiffForDumps($oldDump, $newDump);
+
+            $diff = new DumpDiff();
+            $diff->page_old_dump_id = $oldDump->id;
+            $diff->page_new_dump_id = $newDump->id;
+            $diff->html = $result['html'];
+            $diff->json = $result['json'];
+            $diff->diff_type_id = $result['diff_type_id'];
+
+            $diff->save();
+        }
+    }
+
 
     /**
      * @param Page[] $pages
      * @return void
      */
-    public function calculateDiffForPagesDumps(array $pages): void
+    public function calculateDiffForPages(array $pages): void
     {
         foreach ($pages as $page) {
-            $page->load('lastTwoDumps');
-
-            if ($page->lastTwoDumps->count() === 2) {
-                /** @var PageDump $oldDump */
-                $oldDump = $page->lastTwoDumps[1];
-                /** @var PageDump $newDump */
-                $newDump = $page->lastTwoDumps[0];
-
-                $diff = new DumpDiff();
-                $diff->page_old_dump_id = $oldDump->id;
-                $diff->page_new_dump_id = $newDump->id;
-
-                if ($oldDump->hash === $newDump->hash) {
-                    $diff->html = PlainTextDiffCalculator::EMPTY_HTML;
-                    $diff->json = PlainTextDiffCalculator::EMPTY_JSON;
-                    $diff->diff_type_id = DiffType::NO_CHANGES;
-                } else {
-                    $diff->json = $this->calculateJson($oldDump->raw_pretty_html, $newDump->raw_pretty_html);
-                    $diff->html = $this->calculateHtml($oldDump->raw_pretty_html, $newDump->raw_pretty_html);
-
-                    if ($diff->json === self::EMPTY_JSON || $diff->html === self::EMPTY_HTML) {
-                        $diff->diff_type_id = DiffType::NO_CHANGES;
-                    } else {
-                        $diff->diff_type_id = DiffType::HAS_CHANGES;
-                    }
-                }
-
-                $diff->save();
-            }
+            $this->calculateDiffForPage($page);
         }
     }
 }
